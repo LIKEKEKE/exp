c06e9a40df4ca0f9a335d0d84d9491ce39c1a4c0
LIKEKEKE <568367992@qq.com>
2017-10-08 20:27:14 +0800

    Update main.py

--- hw01/main.py
+++ hw01/main.py
@@ -1,5 +1,8 @@
 for i in range(1,10):
-  for j in range(1,i+1):
-    print('%d * %d'%(i,j,i*j),end='/t')
-  print('/n')
+  for n in range(1,i):
+      print(end='         ')
+  for j in range(i,10):
+    print('%d * %d=%2d'%(j,i,j*i),end=' ')
     
+  print('\n')
+  
