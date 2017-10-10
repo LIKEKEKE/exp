6e32993becc1f439dec0e7eb6ab5c4c2c56d54c7
LIKEKEKE <568367992@qq.com>
2017-10-10 12:59:53 +0800

    Update main.py

--- hw02/main.py
+++ hw02/main.py
@@ -2,10 +2,13 @@ def besearch(data,target):
   long=len(data)-1
   mid=int(long/2)
   high=0
-  while(high<=long):
-  if(data[mid]=target):
-    return(mid)
-  elif(data[mid]>target):
-    mid=mid-1
-  elif(data[mid]<target):
-   mid=mid+1
+  if target in data:
+    while(high<=long):
+      if(data[mid]==target):
+         return(mid)
+      elif(data[mid]>target):
+         mid=mid-1
+       elif(data[mid]<target):
+         mid=mid+1
+    else return none
+  
